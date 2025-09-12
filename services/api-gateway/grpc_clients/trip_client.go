ea12c3e5018cb3f175dbd209c9b66d808eddb589
Yuba Neupane <neopaneyb2@gmail.com>
2025-09-12 10:14:49 -0400

    GRPC

---
+++ services/api-gateway/grpc_clients/trip_client.go
@@ -0,0 +1,42 @@
+package grpc_clients
+
+import (
+	"os"
+	pb "ride-sharing/shared/proto/trip"
+
+	"google.golang.org/grpc"
+)
+
+type tripSericeClient struct {
+	Client pb.TripServiceClient
+	conn   *grpc.ClientConn
+}
+
+func NewTripServiceClient() (*tripSericeClient, error) {
+	tripServiceURL := os.Getenv("TRIP_SERVICE_URL")
+
+	if tripServiceURL == "" {
+		tripServiceURL = "trip-service:9093"
+	}
+
+	conn, err := grpc.NewClient(tripServiceURL)
+	if err != nil {
+		return nil, err
+	}
+
+	client := pb.NewTripServiceClient(conn)
+
+	return &tripSericeClient{
+		conn:   conn,
+		Client: client,
+	}, nil
+
+}
+
+func (c *tripSericeClient) Close() {
+	if c.conn != nil {
+		if err := c.conn.Close(); err != nil {
+			return
+		}
+	}
+}
--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -2,69 +2,56 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"net/http"
+	"net"
 	"os"
 	"os/signal"
-	h "ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
 	"ride-sharing/shared/env"
 	"syscall"
-	"time"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	grpcserver "google.golang.org/grpc"
 )
 
 var (
-	httpAddr = env.GetString("HTTP_ADDR", ":8083")
+	GrpcAddr = env.GetString("GRPC_ADDR", ":9093")
 )
 
 func main() {
 
-	mux := chi.NewRouter()
-	mux.Use(middleware.Logger)
-	mux.Use(middleware.Recoverer)
 	inmemRepo := repository.NewInmemRepository()
+	svc := service.NewService(inmemRepo)
 
-	httpHandler := &h.HttpHandler{
-		Sevice: service.NewService(inmemRepo),
-	}
-
-	fmt.Println("Trip Service is running...")
-
-	mux.Post("/preview", httpHandler.HandlePreview)
-
-	server := &http.Server{
-		Addr:    httpAddr,
-		Handler: mux,
-	}
-
-	serverErrors := make(chan error, 1)
+	ctx, cancel := context.WithCancel(context.Background())
 
 	go func() {
-		log.Printf("Server Listening on %s", httpAddr)
-		serverErrors <- server.ListenAndServe()
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+		<-sigCh
+		cancel()
 	}()
 
-	shutdown := make(chan os.Signal, 1)
+	lis, err := net.Listen("tcp", GrpcAddr)
 
-	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
-
-	select {
-	case err := <-serverErrors:
-		log.Printf("Error starting the server: %v", err)
-	case sig := <-shutdown:
-		log.Printf("Server is shutting down due to %v signal", sig)
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-
-		if err := server.Shutdown(ctx); err != nil {
-			log.Printf("Could not stop server gracefully: %v", err)
-			server.Close()
-		}
+	if err != nil {
+		log.Fatal("failed to listen: %v", err)
 	}
+
+	grpcServer := grpcserver.NewServer()
+
+	log.Printf("Starting gRPC server Trip service on port %s", lis.Addr().String())
+
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Printf("failed to serve %v", err)
+			cancel()
+		}
+	}()
+
+	// wait for the shutdown signal
+	<-ctx.Done()
+	log.Printf("Shutting down the server %v", err)
+	grpcServer.GracefulStop()
 
 }
